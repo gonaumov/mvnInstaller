f3a0a9521907803bf929af3fcdd9cc597dc4e60f
Georgi Naumov <gonaumov@gmail.com>
2016-07-30 20:03:56 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+This is simple project. I have a idea to create reusable function to install maven. 
+This function can be very useful for vagrant provision for example. The only 
+parameter is apache maven binary download url. The function gett all other stuff
+from url string. 
---
+++ mvnInstaller.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -x 
+###################################################################################################
+#   This is reusable function for maven installation.
+#   The function expect one parameter - url maven
+#   binary. Example of usage: 
+#   installMaven "http://apache.cbox.biz/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
+###################################################################################################
+function installMaven {
+	declare -r apacheMavenDownloadUrl=$1
+	declare -r directoryName="mvn/"
+
+	if [[ $apacheMavenDownloadUrl =~ ([^/]+)\-([^\-]+)\-bin\.tar\.gz$ ]]
+	then
+		declare -r resultFileName=$BASH_REMATCH
+		declare -r resultDirectoryName="${BASH_REMATCH[1]}-${BASH_REMATCH[2]}"
+		declare -r mavenVersion=${BASH_REMATCH[2]}
+	else 
+		printf "\nIncorrcet format of Apache Maven download url!\n"
+		exit 1  
+	fi
+    
+    mkdir -p $directoryName
+	cd $directoryName
+	wget $apacheMavenDownloadUrl
+	tar -zxf $resultFileName
+	declare -r workingDir=$(pwd)
+	sudo ln -s "$workingDir/$resultDirectoryName/bin/mvn" /usr/local/bin/mvn
+	rm -v $resultFileName
+	printf "\nMaven $mavenVersion is installed!\n"
+}
+
+installMaven "http://apache.cbox.biz/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
